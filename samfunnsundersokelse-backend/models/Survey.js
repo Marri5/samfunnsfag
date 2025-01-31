9e8023dd481ed4aa142a1878533734f8992bb359
Marri5 <128259249+Marri5@users.noreply.github.com>
2025-01-31 22:15:44 +0100

    .

--- samfunnsundersokelse-backend/models/Survey.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SurveySchema = new mongoose.Schema({
-  question: String,
-  options: [String],
-  responses: [{ option: String, count: Number }]
-});
-
-module.exports = mongoose.model('Survey', SurveySchema);
